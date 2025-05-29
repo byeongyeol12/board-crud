7602a1fd8a557d3b6015c6dff7bcbe09ac4877e3
뺑열 <byeongyeol12@gmail.com>
2025-05-29 18:39:06 +0900

    포스트 엔티티 등록

---
+++ src/main/java/com/example/board/domain/post/entity/Post.java
@@ -0,0 +1,48 @@
+package com.example.board.domain.post.entity;
+
+import com.example.board.domain.base.BaseUpdatableEntity;
+import com.example.board.domain.user.entity.User;
+import jakarta.persistence.*;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseUpdatableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private String id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Post(String title, String content, User user){
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+
+    public void softDelete() {
+        this.isDeleted = true;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
