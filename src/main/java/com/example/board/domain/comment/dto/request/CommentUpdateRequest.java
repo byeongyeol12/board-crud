6c401bbfedd556a6f77dd033f2396799993418d6
뺑열 <byeongyeol12@gmail.com>
2025-06-07 17:44:03 +0900

    댓글 dto 수정

--- src/main/java/com/example/board/domain/comment/dto/request/CommentUpdateRequest.java
+++ src/main/java/com/example/board/domain/comment/dto/request/CommentUpdateRequest.java
@@ -3,7 +3,7 @@ package com.example.board.domain.comment.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record CommentUpdateRequest (
-    @NotBlank(message = "댓글 내용은 필수입니다")
+    @NotBlank(message = "댓글 내용은 필수입니다.")
     String content
 ) {}
 
