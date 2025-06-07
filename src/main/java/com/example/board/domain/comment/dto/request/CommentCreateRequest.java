366de0708adf1bbf98c8b65809ba0578274f715d
뺑열 <byeongyeol12@gmail.com>
2025-06-07 17:27:24 +0900

    댓글 dto 설계

--- src/main/java/com/example/board/domain/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/com/example/board/domain/comment/dto/request/CommentCreateRequest.java
@@ -1,4 +1,18 @@
 package com.example.board.domain.comment.dto.request;
 
-public record CommentCreateRequest() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record CommentCreateRequest(
+        @NotNull(message = "게시글 ID는 필수 입력 항목입니다.")
+        UUID postId,
+
+        @NotNull(message = "사용자 ID는 필수 입력 항목입니다.")
+        UUID userId,
+
+        @NotBlank(message = "댓글 내용은 필수 입력 항목입니다.")
+        String content
+    
+) {}
--- src/main/java/com/example/board/domain/comment/dto/request/CommentUpdateRequest.java
+++ src/main/java/com/example/board/domain/comment/dto/request/CommentUpdateRequest.java
@@ -1,4 +1,9 @@
 package com.example.board.domain.comment.dto.request;
 
-public record CommentUpdateRequest() {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record CommentUpdateRequest (
+    @NotBlank(message = "댓글 내용은 필수입니다")
+    String content
+) {}
+
