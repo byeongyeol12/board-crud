fdf84637537ab86b98523a21a833291115b4a5d7
뺑열 <byeongyeol12@gmail.com>
2025-06-07 17:45:36 +0900

    댓글 서비스, 댓글 컨트롤러 생성

---
+++ src/main/java/com/example/board/domain/comment/controller/CommentController.java
@@ -0,0 +1,4 @@
+package com.example.board.domain.comment.controller;
+
+public class CommentController {
+}
---
+++ src/main/java/com/example/board/domain/comment/service/CommentService.java
@@ -0,0 +1,4 @@
+package com.example.board.domain.comment.service;
+
+public class CommentService {
+}
